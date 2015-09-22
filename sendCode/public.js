3d5f7feced2444e3f6cd7559abd50353079fc4f9
Colco <504105355@qq.com>
2015-09-22 16:33:56 +0800

    Update public.js
    fixed bug for ajax

--- sendCode/public.js
+++ sendCode/public.js
@@ -1,18 +1,20 @@
 /**
  * edit:Cola
- * time:2015/9/11
- * version:1.0
+ * time:2015/9/22
+ * version:1.0.1
  * describe:button for sendCode
  */
 (function($) {
+        //function for sendCode
         $.fn.sendCode = function (options) {
-            var settings = $.extend({
+            var settings = $.extend({},{
                 "time": 30,
                 "phoneVal":"phoneVal",
                 "url":"getCode()"
             },options);
+            console.log(settings);
             phone_obj = $('.'+settings.phoneVal);
-            time = settings.time
+            time = settings.time;
             send_obj = $(this);
             send_obj.click(function () {
                 var val = phone_obj.val();
@@ -22,7 +24,7 @@
                         send_obj.attr('disabled', "disabled");
                         send_obj.css({'color': '#cccccc', 'cursor': 'not-allowed'});
                         //60秒后重新启动发送按钮
-                        forbidButton(phone_obj, send_obj,time);
+                        forbidButton(settings.phoneVal, send_obj,time,settings.url);
                         $.ajax({
                             url: settings.url,
                             data: {'mobile': val},
@@ -37,8 +39,8 @@
                 }
             })
         };
-        function forbidButton(phone_obj, send_obj,time) {
-        //60秒后重新点取验证码
+        function forbidButton(phone_obj, send_obj,time,url) {
+            //60秒后重新点取验证码
             var count = 1;
             send_obj.unbind("click");
             var i = setInterval(function () {
@@ -47,12 +49,17 @@
                     send_obj.html('重新获取');
                     clearInterval(i);
                     send_obj.css({'color': '#000000', 'cursor': 'pointer'});
-                    send_obj.sendCode(phone_obj, send_obj);
+                    send_obj.sendCode({
+                        "time": time,
+                        "phoneVal":phone_obj,
+                        "url":url
+                    });
                 } else {
                     send_obj.html('重新获取' + '(' + parseInt(time - count) + ')');
                 }
                 count++;
             }, 1000);
         }
+
     }
 )(jQuery);
